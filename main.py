5d51a114899fec4822482cc35cbf9e03c7bd331a
spoodr <70950932+IBgreat1@users.noreply.github.com>
2022-05-17 15:26:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from flask import Flask,jsonify
+import bs4
 
 
 app = Flask(__name__)
@@ -9,9 +10,18 @@ print(os.getenv("PORT"))
 @app.route('/LimitedData/<var>')
 def GetLimitedData(var):
   Data = requests.get('https://economy.roblox.com/v1/assets/'+var+'/resale-data')
-  if Data.status_code == 200:
+  Data2 = requests.get('https://www.roblox.com/catalog/'+var)
+  if Data.status_code == 200 and Data2.status_code == 200:
+    document= bs4.BeautifulSoup(Data2.text, 'html.parser')
+    Lowest = document.find("span",class_="text-robux-lg wait-for-i18n-format-render")
+    if Lowest != None:
+      Lowest = Lowest.string
+    else:
+      Lowest = ''
+      
     Data = Data.json()
     Data["success"] = True
+    Data['SellingPrice'] = Lowest
     return jsonify(Data)
   else:
     print(Data.json())
