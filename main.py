a0a49881b954fbdc23e301c1026cb2c401f76794
spoodr <70950932+IBgreat1@users.noreply.github.com>
2022-05-17 11:42:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def GetLimitedData(var):
     Data["success"] = True
     return jsonify(Data)
   else:
+    print(Data.json())
+    print(ProductData.json())
     return jsonify({'success': False})
   
 @app.route("/")
