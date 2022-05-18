b512fc463a94b6ab38a1a1d1ea35da6a2b87953c
spoodr <70950932+IBgreat1@users.noreply.github.com>
2022-05-18 19:21:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 import requests
+import os
+from flask import Flask,jsonify
+import bs4
+from time import sleep
 
-count = 0
-while True:
-    res=requests.get(
-    "https://economy.roblox.com/v1/assets/7592037079/resale-data",
-    proxies={
-        "http": "http://endwaprt-rotate:eh99st1hdkqg@p.webshare.io:80/",
-        "https": "http://endwaprt-rotate:eh99st1hdkqg@p.webshare.io:80/"})
-    if res.status_code == 200:
-        count += 1
-        print(count)
+app = Flask(__name__)
+
+def GetPriceAndName(var):
+  Lowest = None
+  Name = None
+  while Lowest == None and Name == None:
+    Data2 = requests.get('https://www.roblox.com/catalog/'+var)
+    document= bs4.BeautifulSoup(Data2.text, 'html.parser')
+    Lowest = document.find("span",class_="text-robux-lg wait-for-i18n-format-render")
+    Name = document.title
+    if Name == None or Lowest == None:
+      sleep(1)
+  return Lowest.string,Name.string
+
+def GetResaleData(var):
+  Data = None
+  while Data == None :
+    GetData = requests.get('https://economy.roblox.com/v1/assets/'+var+'/resale-data')
+    if GetData.status_code == 200:
+      Data = GetData.json()
     else:
-        print(count,'end')
-        break
+      pass
+      sleep(1)
+  return Data
+
+@app.route('/LimitedData/<var>')
+def GetLimitedData(var):
+    EndData = []
+    for varId in var.split('&'):
+      Data = GetResaleData(varId)
+      Data["success"] = True
+      Data['BestPrice'],Data['Name'] = GetPriceAndName(varId)
+      Data['Id'] = varId
+      
+      EndData.append(Data)
+    return jsonify(EndData)
+  
+@app.route("/")
+def Home():
+  return "Home"
+
+if __name__ == "__main__":
+  app.run()
